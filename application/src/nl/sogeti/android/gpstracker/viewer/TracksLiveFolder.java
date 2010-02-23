ee74d7a041cf5351df49b562a5ed38ecebd36621
Rene de Groot <rcgroot@gmail.com>
2010-02-23 18:26:47 +0000

    Complete - task 77: Add live folder for quick access to tracks","Type-Enhancement, Priority-Medium, Milestone-Release1.1 http://code.google.com/p/open-gpstracker/issues/detail?id=77
    cleanup

--- application/src/nl/sogeti/android/gpstracker/viewer/TracksLiveFolder.java
+++ application/src/nl/sogeti/android/gpstracker/viewer/TracksLiveFolder.java
@@ -3,7 +3,6 @@ package nl.sogeti.android.gpstracker.viewer;
 import nl.sogeti.android.gpstracker.R;
 import nl.sogeti.android.gpstracker.db.GPStracking;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
