4455789723ed83488f99387b7b1d7710895c4f58
Rene de Groot <rcgroot@gmail.com>
2010-06-05 13:09:46 +0000

    Incomplete - task 93: Scrolling is slow on full zoom","Type-Defect, Priority-High, Milestone-Release0.9 http://code.google.com/p/open-gpstracker/issues/detail?id=93

--- test/src/nl/sogeti/android/gpstracker/tests/perf/MapStressTest.java
+++ test/src/nl/sogeti/android/gpstracker/tests/perf/MapStressTest.java
@@ -28,9 +28,19 @@
  */
 package nl.sogeti.android.gpstracker.tests.perf;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 import nl.sogeti.android.gpstracker.db.GPStracking.Tracks;
 import nl.sogeti.android.gpstracker.db.GPStracking.Waypoints;
 import nl.sogeti.android.gpstracker.viewer.LoggerMap;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.net.Uri;
@@ -42,7 +52,7 @@ import android.util.Log;
 
 /**
  * Goal is to feed as the LoggerMap as many points as possible to give it a good workout.
- *
+ * 
  * @version $Id: LoggerMapStressTest.java 47 2009-05-17 19:15:00Z rcgroot $
  * @author rene (c) Mar 15, 2009, Sogeti B.V.
  */
@@ -59,11 +69,11 @@ public class MapStressTest extends ActivityInstrumentationTestCase2<LoggerMap> i
    }
 
    @Override
-   protected void setUp() throws Exception 
+   protected void setUp() throws Exception
    {
       super.setUp();
       getActivity();
-   }  
+   }
 
    protected void tearDown() throws Exception
    {
@@ -71,10 +81,62 @@ public class MapStressTest extends ActivityInstrumentationTestCase2<LoggerMap> i
    }
 
    @LargeTest
-   public void testCreateBigTestTrack()
+   public void testCreateTestData() throws XmlPullParserException, IOException
    {
-      int total = 6000;
+//      createTrackBigTest( 6000 );
+//      createTrackFromKMLData( "/mnt/sdcard/estland50k.xml" );
+   }
+
+   private void createTrackFromKMLData( String xmlResource ) throws XmlPullParserException, IOException
+   {
+
+      XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+      XmlPullParser xpp = factory.newPullParser();
+      xpp.setInput( new FileReader( xmlResource ) );
+
+
+      ContentResolver resolver = this.getActivity().getContentResolver();
+      Uri trackUri = resolver.insert( Tracks.CONTENT_URI, null );
       
+      int eventType = xpp.getEventType();
+      while( eventType != XmlPullParser.END_DOCUMENT )
+      {
+
+         if( eventType == XmlPullParser.START_TAG )
+         {
+            if( "coordinates".equals( xpp.getName() ) )
+            {
+               //Start new Segment
+               Log.d( TAG, "Insert segment " );
+               Uri segmentUri = resolver.insert( Uri.withAppendedPath( trackUri, "segments" ), null );
+               Uri waypointUri = Uri.withAppendedPath( segmentUri, "waypoints" );
+               //Insert all coordinates as waypoints
+               xpp.next();
+               String coords = xpp.getText();
+               StringTokenizer tokizer = new StringTokenizer( coords, " " );
+               String[] tuple = new String[3];
+               String waypoint;
+               ContentValues wp = new ContentValues();
+               while( tokizer.hasMoreTokens() )
+               {
+                  waypoint = tokizer.nextToken();
+                  Log.d( TAG, "Insert waypoint: "+waypoint );
+                  tuple = waypoint.split( "," );
+                  wp.put( Waypoints.LONGITUDE, new Double( tuple[0] ) );
+                  wp.put( Waypoints.LATITUDE, new Double( tuple[1] ) );
+                  wp.put( Waypoints.ALTITUDE, new Double( tuple[2] ) );
+                  resolver.insert( waypointUri, wp );
+               }
+            }
+         }
+         eventType = xpp.next();
+      }
+
+   }
+
+   private void createTrackBigTest( int total )
+   {
       // zig-zag through the netherlands
       double lat1 = 52.195d;
       double lon1 = 4.685d;
@@ -85,51 +147,49 @@ public class MapStressTest extends ActivityInstrumentationTestCase2<LoggerMap> i
 
       ContentResolver resolver = this.getActivity().getContentResolver();
       ContentValues wp = new ContentValues();
-      wp.put( Waypoints.ACCURACY, new Double(10d) );
-      wp.put( Waypoints.ALTITUDE, new Double(5d) );
-      wp.put( Waypoints.SPEED, new Double(15d) );
-      
+      wp.put( Waypoints.ACCURACY, new Double( 10d ) );
+      wp.put( Waypoints.ALTITUDE, new Double( 5d ) );
+      wp.put( Waypoints.SPEED, new Double( 15d ) );
+
       // E.g. returns: content://nl.sogeti.android.gpstracker/tracks/2
       Uri trackUri = resolver.insert( Tracks.CONTENT_URI, null );
       Uri segmentUri = resolver.insert( Uri.withAppendedPath( trackUri, "segments" ), null );
       Uri waypointUri = Uri.withAppendedPath( segmentUri, "waypoints" );
-      
-      for( int step=0;step<total/2;step++ )
+
+      for( int step = 0; step < total / 2; step++ )
       {
-         double latitude = lat1+ ((lat1-lat2)/total)*step;
-         double longtitude = lon1+ ((lon2-lon1)/total)*step;
+         double latitude = lat1 + ( ( lat1 - lat2 ) / total ) * step;
+         double longtitude = lon1 + ( ( lon2 - lon1 ) / total ) * step;
          wp.put( Waypoints.LATITUDE, new Double( latitude ) );
          wp.put( Waypoints.LONGITUDE, new Double( longtitude ) );
          resolver.insert( waypointUri, wp );
       }
-      for( int step=0;step<total/2;step++ )
+      for( int step = 0; step < total / 2; step++ )
       {
-         double latitude = lat2+ ((lat3-lat2)/total)*step;
-         double longtitude = lon2+ ((lon3-lon2)/total)*step;
+         double latitude = lat2 + ( ( lat3 - lat2 ) / total ) * step;
+         double longtitude = lon2 + ( ( lon3 - lon2 ) / total ) * step;
          wp.put( Waypoints.LATITUDE, new Double( latitude ) );
          wp.put( Waypoints.LONGITUDE, new Double( longtitude ) );
          resolver.insert( waypointUri, wp );
       }
    }
-   
-   
+
    /**
-    * Open the first track in the list and scroll around 
-    * forcing redraws during a perf test
+    * Open the first track in the list and scroll around forcing redraws during a perf test
     * 
     * @throws InterruptedException
     */
    @LargeTest
-   public void notestBrowseFirstTrack() throws InterruptedException
+   public void testBrowseFirstTrack() throws InterruptedException
    {
       int actions = 0;
-      String[] timeActions = {"G","G","T","T"};
-      
+      String[] timeActions = { "G", "G", "T", "T" };
+
       // Start method tracing for Issue 18
-      Debug.startMethodTracing("testBrowseFirstTrack");
+      Debug.startMethodTracing( "testBrowseFirstTrack" );
       if( this.mIntermediates != null )
       {
-         this.mIntermediates.startTiming( true ) ;
+         this.mIntermediates.startTiming( true );
       }
       while( actions < timeActions.length )
       {
@@ -139,10 +199,10 @@ public class MapStressTest extends ActivityInstrumentationTestCase2<LoggerMap> i
       }
       if( this.mIntermediates != null )
       {
-         this.mIntermediates.finishTiming( true ) ;
+         this.mIntermediates.finishTiming( true );
       }
       Debug.stopMethodTracing();
-      Log.d( TAG, "Completed actions: "+ actions );
+      Log.d( TAG, "Completed actions: " + actions );
    }
 
    public boolean isPerformanceOnly()
