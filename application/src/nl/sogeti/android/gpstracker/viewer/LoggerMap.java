cdca1667dcb0772b0a9845f6c68914785ae36d51
Rene de Groot <rcgroot@gmail.com>
2010-08-04 13:46:12 +0000

    import cleanup

--- application/src/nl/sogeti/android/gpstracker/viewer/LoggerMap.java
+++ application/src/nl/sogeti/android/gpstracker/viewer/LoggerMap.java
@@ -88,7 +88,6 @@ import android.view.MotionEvent;
 import android.view.SubMenu;
 import android.view.View;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
