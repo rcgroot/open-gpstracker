054fda1bf199daedec037402042079c219eb17ed
Rene de Groot <rcgroot@gmail.com>
2012-01-08 11:49:32 +0000

    Fix hanging bundle and activity dowload in describe activity Fix upload bug introduced in 1.3->1.3.1 Browser to Strict MIME enc

--- application/src/nl/sogeti/android/gpstracker/adapter/tasks/UploadBreadcrumbsTrackTask.java
+++ application/src/nl/sogeti/android/gpstracker/adapter/tasks/UploadBreadcrumbsTrackTask.java
@@ -202,7 +202,7 @@ public class UploadBreadcrumbsTrackTask extends GpxCreator
             throw new IOException("Fail to execute request due to canceling");
          }
          // Build the multipart body with the upload data
-         MultipartEntity entity = new MultipartEntity(HttpMultipartMode.STRICT);
+         MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
          entity.addPart("import_type", new StringBody("GPX"));
          //entity.addPart("gpx",         new FileBody(gpxFile));
          entity.addPart("gpx", new StringBody(gpxString));
@@ -242,7 +242,12 @@ public class UploadBreadcrumbsTrackTask extends GpxCreator
          responseEntity = response.getEntity();
          InputStream stream = responseEntity.getContent();
          responseText = XmlCreator.convertStreamToString(stream);
-
+         
+         if( BreadcrumbsAdapter.DEBUG )
+         {
+            Log.d( TAG, "Upload Response: "+responseText);
+         }
+         
          Pattern p = Pattern.compile(">([0-9]+)</id>");
          Matcher m = p.matcher(responseText);
          if (m.find())
